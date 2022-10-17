70b324aa79c7f618ccd58c3680ad25270d823851
Mtanwork <mtan.uet@gmail.com>
2022-10-17 15:01:19 +0700

    GP1

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 //route
-app.get('/home', (req, res) => {
-  res.send('Hello World!')
+app.get('/homepage', (req, res) => {
+  return res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  return console.log(`Example app listening at http://localhost:${port}`)
 })
